bb2af77fcfbbdcb0d2608e4b194dde7cbe82a888
Vijay <vijaysadhuram4868@gmail.com>
2024-09-01 20:42:46 +0530

    Assertion

--- src/test/java/selenium_webautomation_Learn/ORM_Testing/AssertonDemo.java
+++ src/test/java/selenium_webautomation_Learn/ORM_Testing/AssertonDemo.java
@@ -2,7 +2,12 @@ package selenium_webautomation_Learn.ORM_Testing;
 /*
  *Assertion-validations points
  *
- * 
+ * we can make test method pass or fail
+ * Assert class
+ * Methods
+ * Assert.equa
+ * 1. Hard Assertion
+ * 2.Soft Assertion
  * 
  * 
  * 
@@ -11,6 +16,27 @@ package selenium_webautomation_Learn.ORM_Testing;
  * 
  */
 
-public class AssertonDemo {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+public class AssertonDemo {
+    @Test
+	void testTitle() {
+    
+    	String expected="ho";
+    	String Actual="h";
+    	Assert.assertEquals(expected	, Actual);
+    }
+    @Test
+    void testSoftassertion() {
+        
+    	
+    	String expected="ho";
+    	String Actual="h";
+    	SoftAssert assertObj=new SoftAssert();
+    	assertObj.assertEquals(expected	, Actual);
+    	System.out.println("next statement is ex");
+    	assertObj.assertAll();
+    }
 }
